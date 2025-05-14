48a7eac3b3f13cc718d12d522e12e51bb0920d0b
dar <gown4887@gmail.com>
2025-05-14 22:40:30 +0900

    응답 테스트

--- apps/auth-server/src/auth-server.controller.ts
+++ apps/auth-server/src/auth-server.controller.ts
@@ -1,13 +1,14 @@
-import { Controller } from "@nestjs/common";
-import { MessagePattern } from "@nestjs/microservices";
-import { AuthServerService } from "./auth-server.service";
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { AuthServerService } from './auth-server.service';
 
 @Controller()
 export class AuthServerController {
   constructor(private readonly authServerService: AuthServerService) {}
 
-  @MessagePattern({ cmd: "ping" })
-  ping(): string {
-    return "Auth Microservice is alive";
+  @MessagePattern({ cmd: 'register' })
+  registerUser(data: any): string {
+    // Process the received data
+    return `Received data: ${JSON.stringify(data)}`;
   }
 }
