cf1fe8be2c09a58b13f0e9d36b4d57f73f36103b
dar <gown4887@gmail.com>
2025-05-14 22:38:06 +0900

    불필요한 로직 제거 및 린트 프리티어 적용

--- apps/api-gateway/src/api-gateway.service.ts
+++ apps/api-gateway/src/api-gateway.service.ts
@@ -1,51 +1,34 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { ClientProxy } from "@nestjs/microservices";
-import { firstValueFrom } from "rxjs";
+import { UserRegisterDto } from '@app/common/dtos/user-register.dto';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class ApiGatewayService {
   constructor(
-    @Inject("AUTH_SERVICE") private readonly authClient: ClientProxy,
-    @Inject("EVENT_SERVICE") private readonly eventClient: ClientProxy
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
+    @Inject('EVENT_SERVICE') private readonly eventClient: ClientProxy,
   ) {}
 
-  getHello(): string {
-    return "API Gateway is running";
-  }
-
-  async checkAuthServiceStatus(): Promise<string> {
-    try {
-      return await firstValueFrom(this.authClient.send({ cmd: "ping" }, {}));
-    } catch (error) {
-      return "Auth service unavailable";
-    }
-  }
-
-  async checkEventServiceStatus(): Promise<string> {
-    try {
-      return await firstValueFrom(this.eventClient.send({ cmd: "ping" }, {}));
-    } catch (error) {
-      return "Event service unavailable";
-    }
-  }
-
   // Auth 서비스로 요청 전달
-  async forwardToAuthService(pattern: string, data: any): Promise<any> {
-    try {
-      return await firstValueFrom(this.authClient.send({ cmd: pattern }, data));
-    } catch (error) {
-      this.handleServiceError(error, "Auth");
-    }
+  registUser(pattern: string, data: UserRegisterDto) {
+    return this.forwardToAuthService(pattern, data);
+  }
+
+  private forwardToAuthService(pattern: string, data: UserRegisterDto) {
+    return this.authClient.send({ cmd: pattern }, data).pipe(
+      catchError((error) => {
+        this.handleServiceError(error, 'Auth');
+      }),
+    );
   }
 
   // Event 서비스로 요청 전달
   async forwardToEventService(pattern: string, data: any): Promise<any> {
     try {
-      return await firstValueFrom(
-        this.eventClient.send({ cmd: pattern }, data)
-      );
+      return await firstValueFrom(this.eventClient.send({ cmd: pattern }, data));
     } catch (error) {
-      this.handleServiceError(error, "Event");
+      this.handleServiceError(error, 'Event');
     }
   }
 
@@ -57,7 +40,7 @@ export class ApiGatewayService {
     const errorResponse = {
       statusCode: error.status || 500,
       message: error.message || `${serviceName} service error`,
-      error: error.error || "Internal Server Error",
+      error: error.error || 'Internal Server Error',
     };
 
     throw errorResponse;
