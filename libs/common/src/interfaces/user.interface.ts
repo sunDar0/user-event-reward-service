47323aeb24fb7847f882856fe52154a397d1a833
dar <gown4887@gmail.com>
2025-05-14 22:43:37 +0900

    유저 페이로드 수정

--- libs/common/src/interfaces/user.interface.ts
+++ libs/common/src/interfaces/user.interface.ts
@@ -1,13 +1,7 @@
-export interface UserRequest {
+export class UserPayload {
   id: string;
   username: string;
-  email: string;
   roles: string[];
-}
-
-export interface UserPayload {
-  sub: string; // 사용자 ID
-  username: string;
   email: string;
-  roles: string[];
+  name: string;
 }
