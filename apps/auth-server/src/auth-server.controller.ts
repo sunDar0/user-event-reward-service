f55a70b170024958c529ba04e401c48deb9651cd
dar <gown4887@gmail.com>
2025-05-14 22:58:50 +0900

    로그인 서비스 함수 생성

--- apps/auth-server/src/auth-server.controller.ts
+++ apps/auth-server/src/auth-server.controller.ts
@@ -1,3 +1,4 @@
+import { UserRegisterDto } from '@app/common/dtos/user-register.dto';
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 import { AuthServerService } from './auth-server.service';
@@ -7,7 +8,13 @@ export class AuthServerController {
   constructor(private readonly authServerService: AuthServerService) {}
 
   @MessagePattern({ cmd: 'register' })
-  registerUser(data: any): string {
+  registerUser(data: UserRegisterDto): string {
+    // Process the received data
+    return `Received data: ${JSON.stringify(data)}`;
+  }
+
+  @MessagePattern({ cmd: 'login' })
+  loginUser(data: any): string {
     // Process the received data
     return `Received data: ${JSON.stringify(data)}`;
   }
