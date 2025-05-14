d4240a832f611782088ad6f3f2f34000dfa8ab75
dar <gown4887@gmail.com>
2025-05-14 22:42:16 +0900

    로직 개선

---
+++ libs/common/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt-strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [PassportModule, JwtModule],
+})
+export class AuthModule {}
--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,26 +1,8 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { JwtModule } from "@nestjs/jwt";
-import { CommonService } from "./common.service";
+import { Module } from '@nestjs/common';
+import { CommonService } from './common.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: "dev-env",
-    }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        global: true,
-        secret: configService.get<string>("JWT_SECRET"),
-        signOptions: {
-          expiresIn: configService.get<string>("ACCESS_TOKEN_EXPIRATION"),
-        },
-      }),
-      inject: [ConfigService],
-    }),
-  ],
   providers: [CommonService],
-  exports: [CommonService, JwtModule],
+  exports: [CommonService],
 })
 export class CommonModule {}
