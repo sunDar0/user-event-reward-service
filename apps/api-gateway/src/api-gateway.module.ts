e0b7e66bf71bbbe00a9a5fbdc0b6750a2123bfa4
dar <gown4887@gmail.com>
2025-05-14 22:37:53 +0900

    린트, 프리티어 적용

--- apps/api-gateway/src/api-gateway.module.ts
+++ apps/api-gateway/src/api-gateway.module.ts
@@ -1,39 +1,41 @@
-import { CommonModule, JwtAuthGuard, RolesGuard } from "@app/common";
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { APP_GUARD } from "@nestjs/core";
-import { ClientsModule, Transport } from "@nestjs/microservices";
-import { ApiGatewayController } from "./api-gateway.controller";
-import { ApiGatewayService } from "./api-gateway.service";
+import { CommonModule, JwtAuthGuard, RolesGuard } from '@app/common';
+import { AuthModule } from '@app/common/auth/auth.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ApiGatewayController } from './api-gateway.controller';
+import { ApiGatewayService } from './api-gateway.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: "dev-env",
+      envFilePath: 'dev-env',
       isGlobal: true,
     }),
     CommonModule,
+    AuthModule,
     ClientsModule.registerAsync([
       {
-        name: "AUTH_SERVICE",
+        name: 'AUTH_SERVICE',
         imports: [ConfigModule],
         useFactory: (configService: ConfigService) => ({
           transport: Transport.TCP,
           options: {
-            host: configService.get("AUTH_SERVICE_HOST"),
-            port: parseInt(configService.get("AUTH_SERVICE_PORT")),
+            host: configService.get('AUTH_SERVICE_HOST'),
+            port: parseInt(configService.get('AUTH_SERVICE_PORT')),
           },
         }),
         inject: [ConfigService],
       },
       {
-        name: "EVENT_SERVICE",
+        name: 'EVENT_SERVICE',
         imports: [ConfigModule],
         useFactory: (configService: ConfigService) => ({
           transport: Transport.TCP,
           options: {
-            host: configService.get("EVENT_SERVICE_HOST"),
-            port: parseInt(configService.get("EVENT_SERVICE_PORT")),
+            host: configService.get('EVENT_SERVICE_HOST'),
+            port: parseInt(configService.get('EVENT_SERVICE_PORT')),
           },
         }),
         inject: [ConfigService],
