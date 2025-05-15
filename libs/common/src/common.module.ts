cbd0ba2af5434105faacbe4140f6b52146be0c2a
ted.gwon <ted.gwon@kakaovx.com>
2025-05-15 16:47:00 +0900

    공통 모듈에 사용자 모듈 추가 및 Mongoose 통합: UserModule을 추가하고, Mongoose를 비동기적으로 설정하여 MongoDB 연결을 구성함. AuthModule과 함께 내보내도록 수정하여 코드 구조를 개선함.

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
-  exports: [AuthModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [AuthModule, UserModule],
 })
 export class CommonModule {}
