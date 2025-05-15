b0a7ba25c272ae1e88feb4cf9c275f5fcaf0e22a
ted.gwon <ted.gwon@kakaovx.com>
2025-05-15 13:48:35 +0900

    AuthModule에서 JWT 토큰 만료 시간을 환경 변수로 설정하고, JwtService를 추가하여 의존성 주입을 개선함.

--- libs/common/src/auth/auth.module.ts
+++ libs/common/src/auth/auth.module.ts
@@ -3,20 +3,24 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt-strategy';
+import { JwtService } from './jwt.service';
 
 @Module({
   imports: [
+    ConfigModule,
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '1h' },
+        signOptions: {
+          expiresIn: configService.get<string>('ACCESS_TOKEN_EXPIRATION'),
+        },
       }),
       inject: [ConfigService],
     }),
   ],
-  providers: [JwtStrategy],
-  exports: [PassportModule, JwtModule],
+  providers: [JwtStrategy, JwtService],
+  exports: [JwtService],
 })
 export class AuthModule {}
