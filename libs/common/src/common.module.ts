367bce0aaa767c126ed8039937700f76e6e48bc9
ted.gwon <ted.gwon@kakaovx.com>
2025-05-15 13:54:42 +0900

    익스포트 추가

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { CommonService } from './common.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  providers: [CommonService],
-  exports: [CommonService],
+  imports: [AuthModule],
+  exports: [AuthModule],
 })
 export class CommonModule {}
