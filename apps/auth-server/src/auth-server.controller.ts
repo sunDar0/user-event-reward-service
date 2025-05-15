43563e66aa6cc692230b9bd539a199e0b48d42e3
ted.gwon <ted.gwon@kakaovx.com>
2025-05-15 16:44:12 +0900

    Auth 서버에서 사용자 등록 및 로그인 메서드를 개선하고, AUTH_EVENT_TYPE 상수를 사용하여 코드 가독성을 향상시킴. 프로필 업데이트 메서드의 기본 구조 추가.

--- apps/auth-server/src/auth-server.controller.ts
+++ apps/auth-server/src/auth-server.controller.ts
@@ -1,21 +1,26 @@
+import { AUTH_EVENT_TYPE } from '@app/common';
+import { UserLoginDto } from '@app/common/dtos/user-login.dto';
 import { UserRegisterDto } from '@app/common/dtos/user-register.dto';
 import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthServerService } from './auth-server.service';
 
 @Controller()
 export class AuthServerController {
   constructor(private readonly authServerService: AuthServerService) {}
 
-  @MessagePattern({ cmd: 'register' })
-  registerUser(data: UserRegisterDto): string {
-    // Process the received data
-    return `Received data: ${JSON.stringify(data)}`;
+  @MessagePattern({ cmd: AUTH_EVENT_TYPE.REGISTER })
+  async registerUser(@Payload() data: UserRegisterDto) {
+    return this.authServerService.register(data);
   }
 
-  @MessagePattern({ cmd: 'login' })
-  loginUser(data: any): string {
-    // Process the received data
-    return `Received data: ${JSON.stringify(data)}`;
+  @MessagePattern({ cmd: AUTH_EVENT_TYPE.LOGIN })
+  async loginUser(@Payload() data: UserLoginDto) {
+    return this.authServerService.login(data);
+  }
+
+  @MessagePattern({ cmd: AUTH_EVENT_TYPE.UPDATE_PROFILE })
+  async updateProfile(@Payload() data: { refreshToken: string }) {
+    // return this.authServerService.refreshToken(data.refreshToken);
   }
 }
