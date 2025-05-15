46eb5844b7e77bed68c952aabf6d878179408200
ted.gwon <ted.gwon@kakaovx.com>
2025-05-15 13:43:30 +0900

    린트 프리티어 적용

--- apps/event-server/src/event-server.controller.ts
+++ apps/event-server/src/event-server.controller.ts
@@ -1,13 +1,13 @@
-import { Controller } from "@nestjs/common";
-import { MessagePattern } from "@nestjs/microservices";
-import { EventServerService } from "./event-server.service";
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+import { EventServerService } from './event-server.service';
 
 @Controller()
 export class EventServerController {
   constructor(private readonly eventServerService: EventServerService) {}
 
-  @MessagePattern({ cmd: "ping" })
+  @MessagePattern({ cmd: 'ping' })
   ping(): string {
-    return "Event Microservice is alive";
+    return 'Event Microservice is alive';
   }
 }
--- apps/event-server/src/event-server.module.ts
+++ apps/event-server/src/event-server.module.ts
@@ -1,13 +1,13 @@
-import { CommonModule } from "@app/common";
-import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { EventServerController } from "./event-server.controller";
-import { EventServerService } from "./event-server.service";
+import { CommonModule } from '@app/common';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { EventServerController } from './event-server.controller';
+import { EventServerService } from './event-server.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: "dev-env",
+      envFilePath: 'dev-env',
     }),
     CommonModule,
   ],
