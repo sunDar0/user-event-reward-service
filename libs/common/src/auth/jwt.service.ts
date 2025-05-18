c0faef1fac26757447c619c5465ede0bb3fc0f02
dar <gown4887@gmail.com>
2025-05-18 11:59:18 +0900

    토큰 검증 함수 추가

--- libs/common/src/auth/jwt.service.ts
+++ libs/common/src/auth/jwt.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService as NestJwtService } from '@nestjs/jwt';
+import { UserPayload } from '../interfaces';
 
 @Injectable()
 export class JwtService {
@@ -17,11 +18,11 @@ export class JwtService {
   async generateTokens(payload: any): Promise<{ accessToken: string; refreshToken: string }> {
     const [accessToken, refreshToken] = await Promise.all([
       this.jwtService.signAsync(payload, {
-        secret: this.configService.get<string>('JWT_SECRET'),
+        secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
         expiresIn: this.configService.get<string>('ACCESS_TOKEN_EXPIRATION'),
       }),
       this.jwtService.signAsync(payload, {
-        secret: this.configService.get<string>('JWT_SECRET'),
+        secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
         expiresIn: this.configService.get<string>('REFRESH_TOKEN_EXPIRATION'),
       }),
     ]);
@@ -30,5 +31,15 @@ export class JwtService {
       accessToken,
       refreshToken,
     };
+  }
+
+  async verifyRefreshToken(token: string) {
+    try {
+      return await this.jwtService.verifyAsync<UserPayload>(token, {
+        secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      });
+    } catch (error) {
+      throw new Error('Invalid token');
+    }
   }
 }
