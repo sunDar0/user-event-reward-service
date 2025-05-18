855f86f85455d43cbdd5f8c74c089c45ad3d3eeb
dar <gown4887@gmail.com>
2025-05-18 11:57:42 +0900

    키 분리

--- libs/common/src/auth/auth.module.ts
+++ libs/common/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user';
 import { JwtStrategy } from './jwt-strategy';
 import { JwtService } from './jwt.service';
 
@@ -12,13 +13,14 @@ import { JwtService } from './jwt.service';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_ACCESS_SECRET'),
         signOptions: {
           expiresIn: configService.get<string>('ACCESS_TOKEN_EXPIRATION'),
         },
       }),
       inject: [ConfigService],
     }),
+    UserModule,
   ],
   providers: [JwtStrategy, JwtService],
   exports: [JwtService],
