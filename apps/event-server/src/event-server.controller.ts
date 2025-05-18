a58483daed09435cb22bfd82a9aae1c6a9e2c383
dar <gown4887@gmail.com>
2025-05-18 20:06:01 +0900

    이벤트 생성, 이벤트 전체 조회, 이벤트 아이디 기준 조회 함수 구현

--- apps/event-server/src/event-server.controller.ts
+++ apps/event-server/src/event-server.controller.ts
@@ -1,13 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { CreateEventDto, EVENT_EVENT_TYPE } from '@app/common';
+import { Controller, Logger } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { EventServerService } from './event-server.service';
 
 @Controller()
 export class EventServerController {
+  private readonly logger = new Logger(EventServerController.name);
+
   constructor(private readonly eventServerService: EventServerService) {}
 
-  @MessagePattern({ cmd: 'ping' })
-  ping(): string {
-    return 'Event Microservice is alive';
+  @MessagePattern({ cmd: EVENT_EVENT_TYPE.CREATE_EVENT })
+  async createEvent(@Payload() payload: { eventDto: CreateEventDto; createdBy: string }) {
+    return await this.eventServerService.createEvent(payload.eventDto, payload.createdBy);
+  }
+
+  @MessagePattern({ cmd: EVENT_EVENT_TYPE.GET_ALL_EVENTS })
+  async getAllEvents() {
+    try {
+      const events = await this.eventServerService.getAllEvents();
+      return events;
+    } catch (error) {
+      this.logger.error('Error in getAllEvents:', error);
+      throw error;
+    }
+  }
+
+  @MessagePattern({ cmd: EVENT_EVENT_TYPE.GET_EVENT_BY_ID })
+  async getEventById(@Payload() payload: { id: string }) {
+    return await this.eventServerService.getEventById(payload.id);
   }
 }
--- apps/event-server/src/event-server.service.ts
+++ apps/event-server/src/event-server.service.ts
@@ -1,8 +1,19 @@
+import { CreateEventDto, EventResponseDto, EventService } from '@app/common';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class EventServerService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly eventService: EventService) {}
+
+  async createEvent(createEventDto: CreateEventDto, createdBy: string): Promise<EventResponseDto> {
+    return this.eventService.create(createEventDto, createdBy);
+  }
+
+  async getAllEvents(): Promise<EventResponseDto[]> {
+    return this.eventService.findAll();
+  }
+
+  async getEventById(id: string): Promise<EventResponseDto> {
+    return this.eventService.findById(id);
   }
 }
