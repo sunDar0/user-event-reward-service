7d6c564af58424397c6f01734c3b90d576146826
dar <gown4887@gmail.com>
2025-05-18 11:54:22 +0900

    리프레시 엔드포인트 추가

--- apps/auth-server/src/auth-server.controller.ts
+++ apps/auth-server/src/auth-server.controller.ts
@@ -1,6 +1,6 @@
 import { AUTH_EVENT_TYPE } from '@app/common';
-import { UserLoginDto } from '@app/common/dtos/user-login.dto';
-import { UserRegisterDto } from '@app/common/dtos/user-register.dto';
+import { RegisterUserDto } from '@app/common/dtos';
+import { UserLoginDto } from '@app/common/dtos/user.dto';
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthServerService } from './auth-server.service';
@@ -10,16 +10,21 @@ export class AuthServerController {
   constructor(private readonly authServerService: AuthServerService) {}
 
   @MessagePattern({ cmd: AUTH_EVENT_TYPE.REGISTER })
-  async registerUser(@Payload() data: UserRegisterDto) {
+  async registerUser(@Payload() data: RegisterUserDto) {
     return this.authServerService.register(data);
   }
 
   @MessagePattern({ cmd: AUTH_EVENT_TYPE.LOGIN })
   async loginUser(@Payload() data: UserLoginDto) {
-    return this.authServerService.login(data);
+    return await this.authServerService.login(data);
   }
 
-  @MessagePattern({ cmd: AUTH_EVENT_TYPE.UPDATE_PROFILE })
+  @MessagePattern({ cmd: AUTH_EVENT_TYPE.REFRESH_TOKEN })
+  async refreshToken(@Payload() refreshToken: string) {
+    return await this.authServerService.refreshToken(refreshToken);
+  }
+
+  @MessagePattern({ cmd: AUTH_EVENT_TYPE.UPDATE_ROLES })
   async updateProfile(@Payload() data: { refreshToken: string }) {
     // return this.authServerService.refreshToken(data.refreshToken);
   }
