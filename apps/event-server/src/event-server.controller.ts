edbe70279fb512f0c75a8f22e4d2de8d4d5360ab
dar <gown4887@gmail.com>
2025-05-18 22:40:27 +0900

    이벤트 기준 보상 생성, 보상 조회 추가

--- apps/event-server/src/event-server.controller.ts
+++ apps/event-server/src/event-server.controller.ts
@@ -1,4 +1,5 @@
-import { CreateEventDto, EVENT_EVENT_TYPE } from '@app/common';
+import { CreateEventDto, EVENT_EVENT_TYPE, REWARD_EVENT_TYPE } from '@app/common';
+import { CreateRewardDto } from '@app/common/dtos/reward.dto';
 import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { EventServerService } from './event-server.service';
@@ -26,7 +27,17 @@ export class EventServerController {
   }
 
   @MessagePattern({ cmd: EVENT_EVENT_TYPE.GET_EVENT_BY_ID })
-  async getEventById(@Payload() payload: { id: string }) {
-    return await this.eventServerService.getEventById(payload.id);
+  async getEventById(@Payload() eventId: string) {
+    return await this.eventServerService.getEventById(eventId);
+  }
+
+  @MessagePattern({ cmd: REWARD_EVENT_TYPE.CREATE_REWARD })
+  async createReward(@Payload() payload: { eventId: string; createRewardDto: CreateRewardDto }) {
+    return await this.eventServerService.createReward(payload.eventId, payload.createRewardDto);
+  }
+
+  @MessagePattern({ cmd: REWARD_EVENT_TYPE.GET_REWARDS_BY_EVENT_ID })
+  async getEventRewardsByEventId(@Payload() eventId: string) {
+    return await this.eventServerService.getRewardsByEventId(eventId);
   }
 }
