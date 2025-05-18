d38c649aa5fbce359a3ad284a9a0ba22fec1f86b
dar <gown4887@gmail.com>
2025-05-18 11:36:17 +0900

    엔드포인트 수정, 토큰 갱신 추가

--- apps/api-gateway/src/api-gateway.controller.ts
+++ apps/api-gateway/src/api-gateway.controller.ts
@@ -1,10 +1,14 @@
-import { AUTH_EVENT_TYPE, GenerateSwaggerApiDoc, JwtAuthGuard, Roles, RolesGuard } from '@app/common';
-import { UserLoginDto, UserRegisterDto } from '@app/common/dtos';
+import { AUTH_EVENT_TYPE, GenerateSwaggerApiDoc, JwtAuthGuard, Roles, RolesGuard, UserAuth, UserAuthDto } from '@app/common';
+import { RegisterUserDto, UserLoginDto } from '@app/common/dtos';
 import { AllExceptionsFilter, UnauthorizedExceptionFilter } from '@app/common/exception-filters';
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, UseFilters, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { UserDocument } from '@app/common/user';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Post, Put, Query, Req, UseFilters, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { map } from 'rxjs';
 import { ApiGatewayService } from './api-gateway.service';
+import { response } from './helper/response.helper';
 
+@ApiBearerAuth('jwt')
 @Controller({ path: 'api', version: '1' })
 @UseFilters(AllExceptionsFilter, UnauthorizedExceptionFilter)
 @UseGuards(RolesGuard, JwtAuthGuard)
@@ -16,42 +20,51 @@ export class ApiGatewayController {
   @GenerateSwaggerApiDoc({
     tags: ['Auth'],
     summary: '유저 등록',
-    description: '유저 등록을 위한 API입니다.',
+    description: '유저 등록을 진행합니다.',
     isPublic: true,
-    body: { type: UserRegisterDto },
+    body: { type: RegisterUserDto },
   })
-  async register(@Body() userRegisterDto: UserRegisterDto) {
-    return this.apiGatewayService.registerUser(AUTH_EVENT_TYPE.REGISTER, userRegisterDto);
+  register(@Body() RegisterUserDto: RegisterUserDto) {
+    return this.apiGatewayService
+      .registerUser(AUTH_EVENT_TYPE.REGISTER, RegisterUserDto)
+      .pipe(map((user: UserDocument) => response({ user }, '유저등록이 완료되었습니다.', HttpStatus.CREATED)));
   }
 
   @Post('auth/login')
   @GenerateSwaggerApiDoc({
     tags: ['Auth'],
     summary: '로그인',
-    description: '로그인을 위한 API입니다.',
+    description: '로그인을 시도합니다.',
     isPublic: true,
     body: { type: UserLoginDto },
   })
   async login(@Body() loginDto: UserLoginDto) {
-    return this.apiGatewayService.login(AUTH_EVENT_TYPE.LOGIN, loginDto);
+    return this.apiGatewayService
+      .login(AUTH_EVENT_TYPE.LOGIN, loginDto)
+      .pipe(map(({ accessToken, refreshToken }) => response({ accessToken, refreshToken }, '로그인 성공', HttpStatus.OK)));
   }
 
-  @Post('auth/refresh')
-  @ApiTags('Auth')
-  async refresh(@Body() refreshDto: any) {
-    // return this.apiGatewayService.forwardToAuthService('refresh', refreshDto);
-  }
-
-  @Get('users/me')
-  @ApiTags('Auth')
-  async getUserProfile(@Req() request: any) {
-    const userId = request.user?.id; // JWT 인증 후 user 정보가 request에 추가됨
-    // return this.apiGatewayService.forwardToAuthService('getUserProfile', {});
+  @Get('auth/refresh')
+  @GenerateSwaggerApiDoc({
+    tags: ['Auth'],
+    summary: '토큰 갱신',
+    description: '리프레시 토큰을 활용해 엑세스 토큰을 갱신합니다.',
+    isPublic: true,
+    query: { type: 'string', name: 'refreshToken', description: '리프레시 토큰' },
+  })
+  refreshToken(@Query('refreshToken') refreshToken: string) {
+    console.log('refreshToken', refreshToken);
+    return this.apiGatewayService.refreshAccessToken(AUTH_EVENT_TYPE.REFRESH_TOKEN, refreshToken);
   }
 
   @Roles('ADMIN')
   @Put('users/:userId/roles')
-  @ApiTags('Auth')
+  @GenerateSwaggerApiDoc({
+    tags: ['Auth'],
+    summary: '사용자 권한 수정',
+    description: '사용자의 권한을 수정합니다.',
+    param: { type: 'string', name: 'userId', description: '권한을 수정할 대상자의 식별자' },
+  })
   async updateUserRoles(@Param('userId') userId: string, @Body() rolesDto: any) {
     // return this.apiGatewayService.forwardToAuthService('updateUserRoles', {
     //   userId,
@@ -62,7 +75,12 @@ export class ApiGatewayController {
   // Event 서비스 라우팅
   @Roles('OPERATOR', 'ADMIN')
   @Post('events')
-  @ApiTags('Event')
+  @GenerateSwaggerApiDoc({
+    tags: ['Event'],
+    summary: '이벤트 생성',
+    description: '이벤트를 생성합니다.',
+    body: {},
+  })
   async createEvent(@Body() eventDto: any, @Req() request: any) {
     const userId = request.user?.id;
     // return this.apiGatewayService.forwardToEventService(EVENT_EVENT_TYPE.CREATE_EVENT, {
@@ -73,7 +91,8 @@ export class ApiGatewayController {
 
   @Get('events')
   @ApiTags('Event')
-  async getAllEvents(@Query() query: any) {
+  async getAllEvents(@Query() query: any, @UserAuth() userAuth: UserAuthDto) {
+    console.log('userAuth', userAuth);
     // return this.apiGatewayService.forwardToEventService(EVENT_EVENT_TYPE.GET_ALL_EVENTS, query);
   }
 
