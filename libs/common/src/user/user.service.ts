17954bf66ef5ee9e02212d33061ae51e2de1f98a
dar <gown4887@gmail.com>
2025-05-18 12:01:11 +0900

    유저 서비스 고도화

--- libs/common/src/user/user.service.ts
+++ libs/common/src/user/user.service.ts
@@ -2,8 +2,8 @@ import { HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { UserRegisterDto } from '../dtos';
-import { User } from './user.schema';
+import { RegisterUserDto } from '../dtos';
+import { User, UserDocument } from './user.schema';
 
 @Injectable()
 export class UserService {
@@ -23,38 +23,71 @@ export class UserService {
     }
   }
 
-  async createUser(userRegisterDto: UserRegisterDto): Promise<User> {
+  async createUser(RegisterUserDto: RegisterUserDto): Promise<UserDocument> {
     try {
       // 이메일 중복 체크
-      const existingUser = await this.findUserByEmail(userRegisterDto.email);
+      const existingUser = await this.getUserByEmail(RegisterUserDto.email);
       if (existingUser) {
-        this.logger.warn(`User with email ${userRegisterDto.email} already exists`);
+        this.logger.warn(`User with email ${RegisterUserDto.email} already exists`);
         throw new RpcException({
           message: '이미 등록된 이메일입니다.',
           status: HttpStatus.CONFLICT,
         });
       }
-      const createdUser = new this.userModel(userRegisterDto);
+      const createdUser = new this.userModel(RegisterUserDto);
 
       const savedUser = await createdUser.save();
 
       // 필요한 사용자 정보만 반환
-      return savedUser.toObject();
+      return savedUser;
     } catch (error) {
       this.logger.error('Error in create:', error);
+      throw error;
+    }
+  }
 
+  async updateUser(updateUser: UserDocument) {
+    try {
+      // _id를 기준으로 유저 정보 업데이트
+      await updateUser.save();
+    } catch (error) {
+      this.logger.error('Error in updateUser:', error);
       throw new RpcException({
-        message: '사용자 생성 중 오류가 발생했습니다.',
+        message: '유저 정보 업데이트 중 오류가 발생했습니다.',
         status: HttpStatus.INTERNAL_SERVER_ERROR,
       });
     }
   }
 
-  async findUserByEmail(email: string): Promise<User> {
+  async getUserByEmail(email: string): Promise<UserDocument> {
     try {
       return await this.userModel.findOne({ email }).exec();
     } catch (error) {
       this.logger.error('Error in findUserByEmail:', error);
+      throw new RpcException({
+        message: '사용자 조회 중 오류가 발생했습니다.',
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+      });
+    }
+  }
+
+  async getUserById(id: string): Promise<UserDocument | null> {
+    try {
+      return await this.userModel.findById(id).exec();
+    } catch (error) {
+      this.logger.error('Error in getUserById:', error);
+      throw new RpcException({
+        message: '사용자 조회 중 오류가 발생했습니다.',
+        status: HttpStatus.INTERNAL_SERVER_ERROR,
+      });
+    }
+  }
+
+  async getUserByRefreshToken(_id: string, name: string, refreshToken: string) {
+    try {
+      return await this.userModel.findOne({ _id, name, refreshToken }).exec();
+    } catch (error) {
+      this.logger.error('Error in getUserByRefreshToken:', error);
       throw new RpcException({
         message: '사용자 조회 중 오류가 발생했습니다.',
         status: HttpStatus.INTERNAL_SERVER_ERROR,
