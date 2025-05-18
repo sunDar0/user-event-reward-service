dec670fbba75360ea9d0101cd75e212718984939
dar <gown4887@gmail.com>
2025-05-18 11:55:21 +0900

    벨리데이트 추가, 파서 추가

--- apps/auth-server/src/auth-server.module.ts
+++ apps/auth-server/src/auth-server.module.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@app/common';
-import { UserModule } from '@app/common/user/user.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { validateAuthServiceEnv } from 'apps/api-gateway/src/helper/validate.helper';
 import { AuthServerController } from './auth-server.controller';
+import { AuthServerParser } from './auth-server.parser';
 import { AuthServerService } from './auth-server.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: 'dev-env',
+      validationSchema: validateAuthServiceEnv(),
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
@@ -19,9 +21,8 @@ import { AuthServerService } from './auth-server.service';
       inject: [ConfigService],
     }),
     CommonModule,
-    UserModule,
   ],
   controllers: [AuthServerController],
-  providers: [AuthServerService],
+  providers: [AuthServerService, AuthServerParser],
 })
 export class AuthServerModule {}
