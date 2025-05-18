f27f6ed674f5ef9469b74df1b633c77efe8ef02b
dar <gown4887@gmail.com>
2025-05-18 22:40:34 +0900

    이벤트 서버 서비스에 보상 생성 및 조회 기능 추가

--- apps/event-server/src/event-server.service.ts
+++ apps/event-server/src/event-server.service.ts
@@ -1,12 +1,17 @@
-import { CreateEventDto, EventResponseDto, EventService } from '@app/common';
-import { Injectable } from '@nestjs/common';
+import { CreateEventDto, EventResponseDto, EventService, RewardService } from '@app/common';
+import { CreateRewardDto } from '@app/common/dtos/reward.dto';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class EventServerService {
-  constructor(private readonly eventService: EventService) {}
+  constructor(
+    private readonly eventService: EventService,
+    private readonly rewardService: RewardService,
+  ) {}
 
   async createEvent(createEventDto: CreateEventDto, createdBy: string): Promise<EventResponseDto> {
-    return this.eventService.create(createEventDto, createdBy);
+    return this.eventService.createEvent(createEventDto, createdBy);
   }
 
   async getAllEvents(): Promise<EventResponseDto[]> {
@@ -15,5 +20,30 @@ export class EventServerService {
 
   async getEventById(id: string): Promise<EventResponseDto> {
     return this.eventService.findById(id);
+  }
+
+  async createReward(eventId: string, createRewardDto: CreateRewardDto) {
+    try {
+      const event = await this.eventService.findById(eventId);
+      if (!event) {
+        throw new RpcException({
+          message: '이벤트를 찾을 수 없습니다.',
+          status: HttpStatus.NOT_FOUND,
+        });
+      }
+      const newReward = await this.rewardService.createReward(eventId, createRewardDto);
+      return newReward;
+    } catch (error) {
+      throw new RpcException({
+        message: error.message,
+        status: error.status,
+      });
+    }
+  }
+
+  async getRewardsByEventId(eventId: string) {
+    const rewards = await this.rewardService.getRewardsByEventId(eventId);
+
+    return rewards;
   }
 }
