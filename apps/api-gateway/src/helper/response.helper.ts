372a408cf27be5b434089df58ac2f71f9a04afd1
dar <gown4887@gmail.com>
2025-05-19 22:16:34 +0900

    주석 추가

--- apps/api-gateway/src/helper/response.helper.ts
+++ apps/api-gateway/src/helper/response.helper.ts
@@ -1,6 +1,13 @@
 import { BaseResponseDto } from '@app/common';
 import { plainToInstance } from 'class-transformer';
 
+/**
+ * 응답 형식을 반환하는 함수
+ * @param {T} data 응답 데이터
+ * @param {string} message 응답 메시지
+ * @param {number} statusCode 응답 상태 코드
+ * @returns {BaseResponseDto<T>} 응답 형식
+ */
 export function response<T = any>(data: T, message = '', statusCode = 200): BaseResponseDto<T> {
   const response = plainToInstance(BaseResponseDto, {
     code: 0,
--- apps/api-gateway/src/helper/validate.helper.ts
+++ apps/api-gateway/src/helper/validate.helper.ts
@@ -1,6 +1,10 @@
 import { ApiGatewayEnvConfig, AuthServiceEnvConfig, EventServiceEnvConfig } from '@app/common/interfaces';
 import * as Joi from 'joi';
 
+/**
+ * API 게이트웨이 환경 변수 검증
+ * @returns {Joi.ObjectSchema<ApiGatewayEnvConfig>} API 게이트웨이 환경 변수 검증 스키마
+ */
 export function validateApiGateWayEnv(): Joi.ObjectSchema<ApiGatewayEnvConfig> {
   return Joi.object<ApiGatewayEnvConfig, true>({
     APP_ENV: Joi.string().valid('local', 'test', 'dev', 'devqa', 'stage', 'prod').default('dev').required(),
@@ -14,6 +18,10 @@ export function validateApiGateWayEnv(): Joi.ObjectSchema<ApiGatewayEnvConfig> {
   });
 }
 
+/**
+ * 인증 서비스 환경 변수 검증
+ * @returns {Joi.ObjectSchema<AuthServiceEnvConfig>} 인증 서비스 환경 변수 검증 스키마
+ */
 export function validateAuthServiceEnv(): Joi.ObjectSchema<AuthServiceEnvConfig> {
   return Joi.object<AuthServiceEnvConfig, true>({
     APP_ENV: Joi.string().valid('local', 'test', 'dev', 'devqa', 'stage', 'prod').default('dev').required(),
@@ -27,6 +35,10 @@ export function validateAuthServiceEnv(): Joi.ObjectSchema<AuthServiceEnvConfig>
   });
 }
 
+/**
+ * 이벤트 서비스 환경 변수 검증
+ * @returns {Joi.ObjectSchema<EventServiceEnvConfig>} 이벤트 서비스 환경 변수 검증 스키마
+ */
 export function validateEventServiceEnv(): Joi.ObjectSchema<EventServiceEnvConfig> {
   return Joi.object<EventServiceEnvConfig, true>({
     APP_ENV: Joi.string().valid('local', 'test', 'dev', 'devqa', 'stage', 'prod').default('dev').required(),
