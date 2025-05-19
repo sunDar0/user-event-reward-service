35df60dacabf1c778b79402720367e07c3f9baff
dar <gown4887@gmail.com>
2025-05-19 22:14:07 +0900

    MongoDB 초기화 스크립트 수정: 관리자 인증 추가 및 사용자 정보 변경

--- docker/mongo/init-mongo.js
+++ docker/mongo/init-mongo.js
@@ -1,10 +1,14 @@
 // MongoDB 초기화 스크립트
+db = db.getSiblingDB('admin');
+db.auth('admin', '12121212');
+
+// 데이터베이스 생성 및 전환
 db = db.getSiblingDB('maplestory-msa');
 
 // 사용자 생성
 db.createUser({
-  user: 'maplestory',
-  pwd: 'maplestory123',
+  user: 'user-1',
+  pwd: 'user-1-password',
   roles: [
     {
       role: 'readWrite',
@@ -12,19 +16,3 @@ db.createUser({
     },
   ],
 });
-
-// 인덱스 생성
-db.users.createIndex({ email: 1 }, { unique: true });
-db.users.createIndex({ nickname: 1 }, { unique: true });
-
-db.events.createIndex({ startDate: 1, endDate: 1 });
-db.events.createIndex({ status: 1 });
-
-db.rewards.createIndex({ eventId: 1 });
-db.rewards.createIndex({ remainingQuantity: 1 });
-
-db.rewardRequests.createIndex({ userId: 1 });
-db.rewardRequests.createIndex({ eventId: 1 });
-db.rewardRequests.createIndex({ rewardId: 1 });
-db.rewardRequests.createIndex({ status: 1 });
-db.rewardRequests.createIndex({ createdAt: 1 });
