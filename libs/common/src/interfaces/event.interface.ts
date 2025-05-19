b06bbe621214a25244a4b277876702de055dc790
ted.gwon <ted.gwon@kakaovx.com>
2025-05-19 15:52:53 +0900

    인터페이스 개선

--- libs/common/src/interfaces/event.interface.ts
+++ libs/common/src/interfaces/event.interface.ts
@@ -1,20 +1,33 @@
-import { Types } from 'mongoose';
-import { EVENT_STATUS } from '../events/event.constants';
+import { EVENT_CONDITION_TYPE } from '../events/event.constants';
 
-export interface IEventCondition {
-  type: string;
-  details: Record<string, any>;
+export interface EventConditionDetails {
+  [EVENT_CONDITION_TYPE.LOGIN_STREAK]: {
+    targetCount: number;
+  };
+  [EVENT_CONDITION_TYPE.QUEST_CLEAR]: {
+    questId: string;
+  };
+  [EVENT_CONDITION_TYPE.MONSTER_KILL]: {
+    monsterId: string;
+    targetCount: number;
+  };
+  [EVENT_CONDITION_TYPE.RECOMMEND_COUNT]: {
+    targetCount: number;
+  };
+  [EVENT_CONDITION_TYPE.PURCHASE_COUNT]: {
+    targetCount: number;
+  };
 }
 
-export interface IEvent {
-  _id?: Types.ObjectId;
-  title: string;
-  description: string;
-  startDate: Date;
-  endDate: Date;
-  status: EVENT_STATUS;
-  conditions: IEventCondition;
-  createdBy: Types.ObjectId;
-  createdAt?: Date;
-  updatedAt?: Date;
+export interface EventCondition<T extends EVENT_CONDITION_TYPE = EVENT_CONDITION_TYPE> {
+  type: T;
+  details: EventConditionDetails[T];
+}
+
+export interface CompareData {
+  loginDates?: string[];
+  clearedQuests?: string[];
+  killedMonsters?: { monsterId: string; count: number }[];
+  recommendCount?: number;
+  purchaseCount?: number;
 }
